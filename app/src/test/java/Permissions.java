8bc3d25b677f1920a780cdd93b945d6ac699f999
James Pierce <james.pierce82@stclairconnect.ca>
2017-04-09 11:54:15 -0400

    Added Permission Class
    * Also updated personal Android Studio in this commit

---
+++ app/src/test/java/Permissions.java
@@ -0,0 +1,13 @@
+/**
+ * Created by jamespierce on 2017-04-09.
+ */
+
+public class Permissions {
+
+    private boolean canRead;
+    private boolean canWrite;
+    private boolean canEdit;
+
+
+
+}
