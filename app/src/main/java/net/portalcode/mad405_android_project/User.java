4a3684eb98809127dd9450deb53ab9d8c48cf77f
James Pierce <james.pierce82@stclairconnect.ca>
2017-04-10 16:27:29 -0400

    Added Permissions to User
    * User now stores a permissions object that sets what they can and cannot do

--- app/src/main/java/net/portalcode/mad405_android_project/User.java
+++ app/src/main/java/net/portalcode/mad405_android_project/User.java
@@ -1,5 +1,7 @@
 package net.portalcode.mad405_android_project;
 
+import java.security.Permissions;
+
 /**
  * Created by James Pierce on 3/24/2017.
  */
@@ -10,6 +12,7 @@ public class User {
     private int id;
     private String name;
     private int avatar;
+    private Permissions permissions;
 
     // Constructors
     public User() {
@@ -21,10 +24,11 @@ public class User {
         this.avatar = avatar;
     }
 
-    public User(int id, String name, int avatar) {
+    public User(int id, String name, int avatar, Permissions permissions) {
         this.id = id;
         this.name = name;
         this.avatar = avatar;
+        this.permissions = permissions;
     }
 
     // Getters and Setters
@@ -50,5 +54,13 @@ public class User {
 
     public void setAvatar(int avatar) {
         this.avatar = avatar;
+    }
+
+    public Permissions getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(Permissions permissions) {
+        this.permissions = permissions;
     }
 }
