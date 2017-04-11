935563d00bfeec762b958baa1ce0a0f6f1ab97a1
web <web@amso0055z-17.local>
2017-04-11 13:35:32 -0400

    #7 #28 Added Settings for Profile
    * Settings Menu now loads through the Person icon.
    * Needs Customization options.
    * Needs to be configured up with the server as well.

---
+++ app/src/main/java/net/portalcode/mad405_android_project/SettingsActivity.java
@@ -0,0 +1,25 @@
+package net.portalcode.mad405_android_project;
+
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new ChatPrefFragment()).commit();
+    }
+
+    public static class ChatPrefFragment extends PreferenceFragment {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
+
+    }
+}
