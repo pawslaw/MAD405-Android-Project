514e347cf9fbb893fb6b812c9ee1d9b5acdc3803
James Pierce <james.pierce82@stclairconnect.ca>
2017-04-18 22:19:17 -0400

    Added code to SettingsActivity
    * Adding code to check which theme is in place
    * Adding code to set listen for a change to a preference
    * Adding code to change the preferences value after a change is detected

--- app/src/main/java/net/portalcode/mad405_android_project/SettingsActivity.java
+++ app/src/main/java/net/portalcode/mad405_android_project/SettingsActivity.java
@@ -1,16 +1,55 @@
 package net.portalcode.mad405_android_project;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SettingsActivity extends PreferenceActivity {
 
+    private static final String PREFS_NAME = "prefs";
+    private static final String PREF_NIGHT_MODE = "night_mode";
+    SharedPreferences SP;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        SharedPreferences preferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        boolean useNightMode = preferences.getBoolean(PREF_NIGHT_MODE, false);
+
+//        if(useNightMode) {
+//            setTheme(R.style.AppTheme_Dark_NoActionBar);
+//        }
+
         super.onCreate(savedInstanceState);
         getFragmentManager().beginTransaction().replace(android.R.id.content, new ChatPrefFragment()).commit();
+        SP = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        SP.registerOnSharedPreferenceChangeListener(preferencesChangeListener);
+    }
+
+    // This listens for changes to values in Shared Preferences
+    SharedPreferences.OnSharedPreferenceChangeListener preferencesChangeListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+
+            if(key.equals("theme")){
+                Boolean themeChoice = SP.getBoolean(key, false);
+                toggleTheme(themeChoice);
+            }
+        }
+    };
+
+    private void toggleTheme(boolean nightMode) {
+        SharedPreferences.Editor editor = getSharedPreferences(PREFS_NAME, MODE_PRIVATE).edit();
+        editor.putBoolean(PREF_NIGHT_MODE, nightMode);
+        editor.apply();
+
+        Intent intent = new Intent(this, SettingsActivity.class);
+        finish();
+        startActivity(intent);
     }
 
     public static class ChatPrefFragment extends PreferenceFragment {
